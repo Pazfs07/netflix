50a7f3d5c215916e1281621514ee54e28a7b1446
fercho-05 <fperezalpizar@gmail.com>
2023-02-07 13:02:40 -0600

    CreaciónProyectoyFolders || html/css para LoginyUsuarios

---
+++ TareaGrupalNetflix/src/main/java/com/TareaGrupalNetflix/TareaGrupalNetflixApplication.java
@@ -0,0 +1,13 @@
+package com.TareaGrupalNetflix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TareaGrupalNetflixApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TareaGrupalNetflixApplication.class, args);
+	}
+
+}
