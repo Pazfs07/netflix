11c0f23f182314e4ab490e7e7ae71e1d26b9727d
fercho-05 <fperezalpizar@gmail.com>
2023-02-08 17:17:24 -0600

    LoginyUsuarios listos

---
+++ TareaGrupalNetflix/src/test/java/com/TareaGrupalNetflix/TareaGrupalNetflixApplicationTests.java
@@ -0,0 +1,13 @@
+package com.TareaGrupalNetflix;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TareaGrupalNetflixApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
